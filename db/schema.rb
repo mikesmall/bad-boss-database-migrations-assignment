c70e57e68b6f0517bbec3a58dde0a7ca4f75037f
mikesmall <michael.l.a.small@gmail.com>
2017-06-14 19:11:17 -0400

    Completed step 'Adding Quantity to Parts'

--- db/schema.rb
+++ db/schema.rb
@@ -10,12 +10,13 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20170614224637) do
+ActiveRecord::Schema.define(version: 20170614230638) do
 
   create_table "parts", force: :cascade do |t|
     t.string  "name"
     t.string  "manufacturer"
     t.decimal "cost"
+    t.integer "quantity"
   end
 
 end
