797cf8f653420de42c480f12251f9dc105effa2d
mikesmall <michael.l.a.small@gmail.com>
2017-06-14 21:02:53 -0400

    Assignment complete, except that I couldn't restore the locations table, due to the way I altered it previously

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20170615004017) do
+ActiveRecord::Schema.define(version: 20170615005650) do
 
   create_table "locations", force: :cascade do |t|
     t.string "city_name"
@@ -22,18 +22,6 @@ ActiveRecord::Schema.define(version: 20170615004017) do
     t.string  "manufacturer"
     t.decimal "cost"
     t.decimal "quantity"
-  end
-
-  create_table "widgets", force: :cascade do |t|
-    t.string  "first_name"
-    t.string  "last_name"
-    t.text    "dietary_restrictions"
-    t.decimal "salary"
-    t.integer "number_of_kids"
-    t.text    "vulnerabilities"
-    t.text    "illnesses"
-    t.text    "medication"
-    t.text    "voting_preferences"
   end
 
 end
